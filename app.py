19f6a686a4bf2d6d4963b5acaad2ac4223cda37c
Vcaice25 <135193920+Vcaice25@users.noreply.github.com>
2024-01-08 23:17:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,38 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template 
+import requests 
 from dotenv import load_dotenv, dotenv_values
 
-
-config = dotenv_values ('.env') 
+config = dotenv_values ('.env')
 load_dotenv('.env')
-
 
 app = Flask (__name__)
 
 def get_weather_data(city):
-    API_KEY = config ['API_KEY']
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang&appid={API_KEY}'
-    
-    r = requests.get (url).json()
+    API_KEY = config['API_KEY']
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=es&appid={API_KEY}'
+
+    r = requests.get(url).json()
     print (r)
     return r
-
+    
 @app.route('/prueba')
-def prueba ():
-    clima=get_weater_data('Guayaquil')
-    temperatura=str (clima['main']['temp'] )
-    descripcion =str(clima['weather'][0]['description'])
-    icono=str(clima['weather'] [0]['icon'])
+def prueba():
+    clima=get_weather_data('London')
+    temperatura=str(clima['main']['temp'])
+    descripcion= str(clima['weather'][0]['description'])
+    icono=str(clima['weather'][0]['icon'])
     r_json={
-        'ciudad': 'Guayaquil',
+        'ciudad': 'London',
         'temperatura':temperatura,
-        'descripcion':descripcion,
-        'icono':icono
+        'descripcion': descripcion,
+        'icono': icono
         }
-     return render_template('weather.html' ,clima= r_json)  
-   
-
-
-
-
-
-@app.route('/about')
-def about ():
-    return render_template('about.html')
-
-@app.route ('/clima')
-def clima ():
-    return'clima'
+    return render_template('weather.html', clima = r_json)
+ 
+@app.route('/clima')
+def clima():
+    return'Clima'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
+
